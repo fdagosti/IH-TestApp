b3c1a238a6ddfc9a85da5bdf68e635780860ae9f
fdagosti <francois.dagostini@gmail.com>
2016-11-07 20:57:32 +0100

    corrected links in categories

--- public/src/app/common/services/categories.service.js
+++ public/src/app/common/services/categories.service.js
@@ -10,12 +10,35 @@
     var listCategories = function(){
       return $http.get(settings.getCurrentSandbox().url + "categories",  {
         headers: settings.getSandboxHeaders(),
+        }).then(function(response){
+          console.log("categories ", response.data.categories);
+          _correctLinks(response.data.categories);
+          return response;
         });
+    };
+
+    // links are buggy as they do not include the full category Id
+    // We are correcting this at this level temporarily
+    var _correctLinks = function(cats){
+      for (var i = 0; i < cats.length; i++) {
+        var cat = cats[i];
+        var id = cat.id;
+        if (cat._links.self){
+          cat._links.self.href = "/categories/"+id;
+        }
+        if (cat._links.content){
+          cat._links.content.href = "/content?categoryId="+id;
+        }
+      }
     };
 
     var getCategory = function(categoryId){
       return $http.get(settings.getCurrentSandbox().url + "categories/"+categoryId,  {
         headers: settings.getSandboxHeaders(),
+        }).then(function(response){
+          console.log("categories ", response.data.categories);
+          _correctLinks(response.data.categories);
+          return response;
         });
     };
 
